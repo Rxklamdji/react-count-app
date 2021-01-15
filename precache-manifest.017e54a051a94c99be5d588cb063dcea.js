ba4473cbdab8e69d4c10eaa88fcdb3ff1ff918e1
Romeo Klamadji <rklamadji@gmail.com>
2021-01-15 11:57:10 -0600

    Updates

---
+++ precache-manifest.017e54a051a94c99be5d588cb063dcea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbc8de7c983b44bf8a970b47fb4c494",
+    "url": "/react-count-app/index.html"
+  },
+  {
+    "revision": "7ffc67555f99baa99efc",
+    "url": "/react-count-app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "1104bd9c9c388374e30d",
+    "url": "/react-count-app/static/css/main.ea404fa5.chunk.css"
+  },
+  {
+    "revision": "7ffc67555f99baa99efc",
+    "url": "/react-count-app/static/js/2.448b2c42.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-count-app/static/js/2.448b2c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1104bd9c9c388374e30d",
+    "url": "/react-count-app/static/js/main.1cb6a4a4.chunk.js"
+  },
+  {
+    "revision": "04db7a30aa42438a0a02",
+    "url": "/react-count-app/static/js/runtime-main.6a38ba53.js"
+  }
+]);
